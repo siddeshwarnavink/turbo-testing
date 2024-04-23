a72a3a1373dd963203ec2942a53efe108755dd22
Siddeshwar <siddeshwar.work@gmail.com>
2024-04-23 20:33:50 +0530

    enum update

--- src/main/java/sidd33/turbotesting/TestForm.java
+++ src/main/java/sidd33/turbotesting/TestForm.java
@@ -14,11 +14,11 @@ import sidd33.turboengine.forms.type.FormData;
 import sidd33.turboengine.forms.type.FormFieldType;
 
 public class TestForm implements FormData {
-    @FormField(label = "Name", fieldType = FormFieldType.TEXTINPUT)
+    @FormField(label = "Name", fieldType = FormFieldType.TEXT)
     @NotEmpty(message = "Name is required")
     private String name;
 
-    @FormField(label = "Date of birth", fieldType = FormFieldType.DATETIME)
+    @FormField(label = "Date of birth", fieldType = FormFieldType.DATE)
     @NotNull(message = "DOB is required")
     @Min(value = 1, message = "DOB is required")
     private Long dob;
@@ -27,12 +27,12 @@ public class TestForm implements FormData {
     @NotEmpty(message = "Bio is required")
     private String bio;
 
-    @FormField(label = "Image file", fieldType = FormFieldType.FILEINPUT)
+    @FormField(label = "Image file", fieldType = FormFieldType.FILE)
     @MinFileSize(message = "Image is required")
     @MaxFileSize(max = 512_000, message = "Image should not exceed 500kb")
     private MultipartFile file;
 
-    @FormField(label = "Multiple file", fieldType = FormFieldType.FILEINPUT)
+    @FormField(label = "Multiple file", fieldType = FormFieldType.FILE)
     @MaxFileSize(max = 512_000, message = "Images should not exceed 500kb") 
     @MinFileCount(length = 2, message = "Atleast 2 file is needed")
     @MaxFileCount(length = 4, message = "We don't need more than 4 files")
